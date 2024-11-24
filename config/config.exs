c678770b8c3a7242da431d7d28dc3e8afa97071d
Tim <tim@gigalixir.com>
2024-11-24 13:18:04 +0100

    feat: add in main_proxy

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,48 @@
 # is restricted to this project.
 
 # General application configuration
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
 import Config
+
+config :proxy,
+  generators: [timestamp_type: :utc_datetime]
+
+# Configures the endpoint
+config :proxy, ProxyWeb.Endpoint,
+  url: [host: "localhost"],
+  adapter: Bandit.PhoenixAdapter,
+  render_errors: [
+    formats: [json: ProxyWeb.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: Proxy.PubSub,
+  live_view: [signing_salt: "/s/peG++"],
+  server: false
+
+config :main_proxy, http: [port: 4000]
+
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :proxy, Proxy.Mailer, adapter: Swoosh.Adapters.Local
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
 
 config :veggies,
   generators: [timestamp_type: :utc_datetime]
@@ -35,7 +76,8 @@ config :veggies, VeggiesWeb.Endpoint,
     layout: false
   ],
   pubsub_server: Veggies.PubSub,
-  live_view: [signing_salt: "GOoTgD+A"]
+  live_view: [signing_salt: "8aLMvnOz"],
+  server: false
 
 # Configures the mailer
 #
@@ -54,10 +96,6 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{config_env()}.exs"
-
 config :fruits,
   generators: [timestamp_type: :utc_datetime]
 
@@ -70,7 +108,8 @@ config :fruits, FruitsWeb.Endpoint,
     layout: false
   ],
   pubsub_server: Fruits.PubSub,
-  live_view: [signing_salt: "1JCdqbV1"]
+  live_view: [signing_salt: "k5yYCmCi"],
+  server: false
 
 # Configures the mailer
 #
